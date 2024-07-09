7e3f2a9e648f5ca56581e1a35bc9a953bf803df9
marcusungarette <marcusungarette@gmail.com>
2024-07-09 18:25:59 -0300

    added new DTO to deal with cadastro paciente

--- api/src/main/java/med/agi/api/paciente/DadosCadastroPaciente.java
+++ api/src/main/java/med/agi/api/paciente/DadosCadastroPaciente.java
@@ -1,2 +1,12 @@
-package med.agi.api.paciente;public record DadosCadastroPaciente() {
+package med.agi.api.paciente;
+
+import med.agi.api.endereco.DadosEndereco;
+
+public record DadosCadastroPaciente(
+        String nome,
+        String email,
+        String telefone,
+        String cpf,
+        DadosEndereco endereco
+) {
 }
