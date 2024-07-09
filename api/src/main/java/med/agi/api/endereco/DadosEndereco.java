c0ea81385e54f771cb98986ad5fce9530bbfa51d
marcusungarette <marcusungarette@gmail.com>
2024-07-09 17:24:44 -0300

    added DTOs to aplication

---
+++ api/src/main/java/med/agi/api/endereco/DadosEndereco.java
@@ -0,0 +1,4 @@
+package med.agi.api.endereco;
+
+public record DadosEndereco(String logradouro, String bairro, String cep, String uf, String complemento, String numero) {
+}
---
+++ api/src/main/java/med/agi/api/medico/DadosCadastroMedico.java
@@ -0,0 +1,6 @@
+package med.agi.api.medico;
+
+import med.agi.api.endereco.DadosEndereco;
+
+public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade, DadosEndereco endereco) {
+}
