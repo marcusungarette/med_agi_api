8478e8955fe87131c8e2333d2ce5bc27014adb6f
marcusungarette <marcusungarette@gmail.com>
2024-07-09 19:05:11 -0300

    added field cidade was missing

--- api/src/main/java/med/agi/api/endereco/DadosEndereco.java
+++ api/src/main/java/med/agi/api/endereco/DadosEndereco.java
@@ -1,4 +1,4 @@
 package med.agi.api.endereco;
 
-public record DadosEndereco(String logradouro, String bairro, String cep, String uf, String complemento, String numero) {
+public record DadosEndereco(String logradouro, String bairro, String cidade, String cep, String uf, String complemento, String numero) {
 }
