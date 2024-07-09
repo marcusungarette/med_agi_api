baaccf322da3d78e3ccb541980738b95472ea92a
marcusungarette <marcusungarette@gmail.com>
2024-07-09 19:07:54 -0300

    create dependency injection on MedicoController using MedicoRepository, saving data on database

--- api/src/main/java/med/agi/api/controller/MedicoController.java
+++ api/src/main/java/med/agi/api/controller/MedicoController.java
@@ -1,6 +1,9 @@
 package med.agi.api.controller;
 
 import med.agi.api.medico.DadosCadastroMedico;
+import med.agi.api.medico.Medico;
+import med.agi.api.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("medicos")
 public class MedicoController {
 
+    @Autowired // Dependency Injection
+    private MedicoRepository repository;
+
     @PostMapping
     public void cadastrar(@RequestBody DadosCadastroMedico dados) {
-        System.out.println(dados);
+        repository.save(new Medico(dados));
 
     }
 }
