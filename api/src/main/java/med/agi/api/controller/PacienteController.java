349c7bfbb0fc53b8fd1e9d1fa9ec0cbfa92d5a3c
marcusungarette <marcusungarette@gmail.com>
2024-07-09 21:03:22 -0300

    added DTOs and annotations to create new table Pacientes

--- api/src/main/java/med/agi/api/controller/PacienteController.java
+++ api/src/main/java/med/agi/api/controller/PacienteController.java
@@ -1,6 +1,11 @@
 package med.agi.api.controller;
 
+import jakarta.validation.Valid;
 import med.agi.api.paciente.DadosCadastroPaciente;
+import med.agi.api.paciente.Paciente;
+import med.agi.api.paciente.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("pacientes")
 public class PacienteController {
 
-    @PostMapping
-    public void cadastrar(@RequestBody DadosCadastroPaciente dados) {
-        System.out.println("dados recebido: " +dados);
-    }
+    @Autowired
+    private PacienteRepository repository;
 
+    @PostMapping
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados) {
+        repository.save(new Paciente(dados));
+    }
 }
+
