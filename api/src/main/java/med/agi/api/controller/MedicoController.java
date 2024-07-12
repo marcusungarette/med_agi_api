52a383c2b4260f29d57c05fd565d2fb96c709226
marcusungarette <marcusungarette@gmail.com>
2024-07-12 06:58:22 -0300

    added DTOs and new method GET to return All medicos list from database

--- api/src/main/java/med/agi/api/controller/MedicoController.java
+++ api/src/main/java/med/agi/api/controller/MedicoController.java
@@ -2,14 +2,14 @@ package med.agi.api.controller;
 
 import jakarta.validation.Valid;
 import med.agi.api.medico.DadosCadastroMedico;
+import med.agi.api.medico.DadosListagemMedico;
 import med.agi.api.medico.Medico;
 import med.agi.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
@@ -22,6 +22,10 @@ public class MedicoController {
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
         repository.save(new Medico(dados));
+    }
 
+    @GetMapping
+    public List<DadosListagemMedico> listar(){
+        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
     }
 }
