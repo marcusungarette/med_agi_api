ce65d3fbafc5659cfabc030675599b842f595203
marcusungarette <marcusungarette@gmail.com>
2024-07-14 18:22:16 -0300

    Added a new class ErrorHandler - deal with 404 and 400 return message correct to client

---
+++ api/src/main/java/med/agi/api/infra/ErrorHandler.java
@@ -0,0 +1,31 @@
+package med.agi.api.infra;
+
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity errorHandler404(){
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity erroHandler400(MethodArgumentNotValidException exception){
+        var error = exception.getFieldErrors();
+
+        return ResponseEntity.badRequest().body(error.stream().map(DadosErroValidacaoDTO:: new).toList());
+    }
+
+    private record DadosErroValidacaoDTO(String campo, String mensagem) {
+        public DadosErroValidacaoDTO(FieldError error) {
+            this(error.getField(), error.getDefaultMessage());
+        }
+    }
+}
